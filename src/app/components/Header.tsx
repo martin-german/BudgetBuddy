52b15092ede55413825baf9ce0778d9f61174760
GermánMartin <martingerman991@gmail.com>
2025-08-06 16:49:23 +0200

    @Added MaterialUI, changed favico, created header.

---
+++ src/app/components/Header.tsx
@@ -0,0 +1,76 @@
+import SavingsOutlinedIcon from "@mui/icons-material/SavingsOutlined";
+import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
+import AddOutlinedIcon from "@mui/icons-material/AddOutlined";
+
+
+function Header() {
+  return (
+    <nav className="flex justify-between items-center">
+      {/*Logo Section*/}
+      <LogoSection />
+
+      {/*Search Bar*/}
+      <SearchBar />
+
+      {/*Button */}
+      <Button />
+    </nav>
+  );
+}
+
+export default Header;
+
+function LogoSection() {
+  return (
+    <div className="flex gap-2 items-center">
+      {/*Icon container */}
+      <div className="bg-teal-500 flex items-center justify-center p-[11px] rounded-lg">
+        {/*Piggy Icon*/}
+        <div className="flex w-[36px] h-[36px] items-center justify-center">
+          <SavingsOutlinedIcon
+            sx={{ fontSize: "30px" }}
+            className="text-gray-800 "
+          />
+        </div>
+      </div>
+
+      <div className="flex gap-1 text-[24px]">
+        <h1 className="text-4xl font-black text-black">
+          Budget
+          <span className="font-thin text-zinc-900">Buddy</span>
+        </h1>
+      </div>
+    </div>
+  );
+}
+
+function SearchBar() {
+
+  return (
+    <div
+      className="h-[46px] bg-slate-100 flex items-center text-sm 
+    rounded-md pl-3 gap-1 w-[300px]">
+      <div>
+        <SearchOutlinedIcon className="text-black" />
+      </div>
+      <input
+        type="text"
+        placeholder="Search an expense..."
+        className="bg-transparent outline-none w-full font-light"
+      />
+    </div>
+  );
+}
+
+function Button() {
+  return (
+    <button
+      className="gap-1 p-[12px] flex text-sm rounded-md 
+    font-black text-black bg-transparent border border-black hover:bg-teal-500 
+    items-center justify-center pr-[18px] 
+    ">
+      <AddOutlinedIcon sx={{ fontSize: "20px" }} />
+      <span className="">New Expense</span>
+    </button>
+  );
+}
---
+++ src/app/components/SubHeader.tsx
@@ -0,0 +1,5 @@
+function Subheader() {
+  <div className="mt-[90px] flex justify-between"></div>;
+}
+
+export default Subheader;
